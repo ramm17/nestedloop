6b01561ef0558d315bbb63f0d172f01f6e5b0710
Ramm <ramgopal.rana.161@gmail.com>
2022-11-21 19:49:55 +0545

    New files uploaded

---
+++ ASCII3.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    char ch='A';
+    int i,j,r;
+    printf("Enter the no of rows:");
+    scanf("%d",&r);
+    for(i=1;i<=r;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("%c ",ch);
+            ch++;
+        }
+        ch='A';
+        printf("\n");
+    }
+    getch();
+    return 0;
+}
